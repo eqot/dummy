57fdbfd708cb8135a4234dc7b3bc4f941495d8c9
Ikuo Terado <eqobar@gmail.com>
2019-04-24 16:59:22 +0900

    Minor fix

--- packages/dummy-doc/website/siteConfig.js
+++ packages/dummy-doc/website/siteConfig.js
@@ -82,7 +82,7 @@ const siteConfig = {
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['/dummy/js/dummy-core.js', 'https://buttons.github.io/buttons.js'],
+  scripts: ['../js/dummy-core.js', 'https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
